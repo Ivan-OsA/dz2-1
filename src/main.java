1d61cdaab6196b5aa53433dbca21a4a493773d0b
Иван Осокин <osokin_ivan1992@mail.ru>
2022-07-05 21:29:18 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 3170;
+        int x = 20;
+        int i;
+        i = price / x;
+        System.out.println(i);
+
+    }
+
+}
